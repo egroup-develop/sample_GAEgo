6be144c3d13278f148c4eeb5c2aa6d769f9fd7e2
suzukakedaiki <karahumwmmt@gmail.com>
2015-11-23 10:43:41 +0900

    引数持ってこれないやつ

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-function SampleController($scope) {
-	$scope.click = function() {
-		$scope.message1 = 'you clicked left';
-		$scope.message2 = 'you clicked right';
-	};
-}
+var app = angular.module('myApp', []);
+app.controller('mainCtrl', ['$scope', function($scope) {
+  $scope.items = [
+    {key: 1, value: "left"},
+    {key: 2, value: "right"}
+  ];
+  $scope.ClickFunction = function(val) {
+      angular.element(document.querySelector('#value')).text(val);
+    }
+}]);}
