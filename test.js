55bd67c1ec500cf629e25d6bb400303a2e54c241
suzukakedaiki <karahumwmmt@gmail.com>
2015-11-30 12:38:46 +0900

    比較sample(localjson)

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-function SampleController($scope) {
-	$scope.click = function() {
-		$scope.message1 = 'you clicked left';
-		$scope.message2 = 'you clicked right';
-	};
-}
+var app = angular.module('myApp', []);
+app.controller('mainCtrl', ['$scope', function($scope) {
+  $scope.items = [
+    {key: 1, value: "伊藤里織",fileName:"saori.jpg"},
+    {key: 2, value: "佐野ひなこ",fileName:"hinako.jpg"}
+  ];
+  $scope.ClickFunction = function(val) {
+      angular.element(document.querySelector('#value')).text(val);
+    }
+}]);
