b06e1ef9e65837c2c0c1c0cf01afbcf43e16b04c
suzukakedaiki <karahumwmmt@gmail.com>
2015-11-30 12:32:14 +0900

    2nd picture

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var app = angular.module('myApp', []);
 app.controller('mainCtrl', ['$scope', function($scope) {
   $scope.items = [
-    {key: 1, value: "left"},
-    {key: 2, value: "right"}
+    {key: 1, value: "伊藤里織",fileName:"saori.jpg"},
+    {key: 2, value: "佐野ひなこ",fileName:"hinako.jpg"}
   ];
   $scope.ClickFunction = function(val) {
       angular.element(document.querySelector('#value')).text(val);
     }
-}]);}
+}]);
