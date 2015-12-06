c4114c81127e81ef1bea2fb7c56f6f25127c1d94
suzukakedaiki <karahumwmmt@gmail.com>
2015-12-06 11:29:03 +0900

    localでjsonからの読みこみtest

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 var app = angular.module('myApp', []);
-app.controller('mainCtrl', ['$scope', function($scope) {
-  $scope.items = [
-    {key: 1, value: "伊藤里織",fileName:"saori.jpg"},
-    {key: 2, value: "佐野ひなこ",fileName:"hinako.jpg"}
-  ];
-  $scope.ClickFunction = function(val) {
-      angular.element(document.querySelector('#value')).text(val);
-    }
-}]);
+app.controller('mainCtrl', function($scope,$http) {
+	/*$scope.items = [{"Id":"test"}];*/
+	$scope.getUsersFromLocal = function() {
+		$http({method: 'GET', url:'http://localhost:8000/out.json', headers:{"Content-Type":'application/json'}}).
+	success(function(data) {
+		$scope.items = data;
+	});
+	};
+
+	$scope.getUsersFromLocal(); //call ajax method
+});
+/*$scope.ClickFunction = function(val) {
+	angular.element(document.querySelector('#Id')).text(val);
+}
+}]);*/
