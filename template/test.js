fc140628d732dfda485abdd73ed2f7db162523ee
suzukakedaiki <karahumwmmt@gmail.com>
2016-01-10 12:16:42 +0900

    安部jsonから取得後。必要なデータのみjsに送信

--- template/test.js
+++ template/test.js
@@ -1,6 +1,7 @@
-var id_arr = new Array();
-var choise_arr = new Array();
-var count = 0;
+var id_arr = new Array();	//選択された方のidを保管
+var index_arr = new Array();	//選択画面の二人の組み合わせのidを保管
+var choise_arr = new Array();	//jsonから取得したデータ
+var count = 0;			
 var items = new Array();
 
 var app = angular.module('myApp', []);
@@ -8,17 +9,15 @@ app.controller('mainCtrl', function($scope,$http) {
   $scope.getUsersFromLocal = function() {
     $http({
       method: 'GET',
-      url: 'https://study-golang.appspot.com/outjson?myname=hiroki&portalname=logirl&start=10&limit=12&category=Daily%2BLoGiRL',
+      url: 'http://localhost:8080/template/save_person_details.json',
       headers: {"Content-Type":'application/json'}
     }).
     success(function(data) {
-      // $scope.items = data;
       items = data;
-      for(i=0;i<2;i++){
-        choise_arr.push({"Url":data[i].Url[0] ,"Title":data[i].Title,"Id":data[i].Id});
+      for(i=count;i<(count+2);i++){
+        choise_arr.push({"ImageUrl":data[i].ImageUrl[0] ,"Name":data[i].Name,"Index":data[i].Index});
       }
-      count++;
-      console.log("choise:" + choise_arr);
+      count = count + 2;
       $scope.choise = choise_arr;
     });
   };
@@ -28,29 +27,34 @@ app.controller('mainCtrl', function($scope,$http) {
 
   $scope.ClickFunction = function(val,index) {
     angular.element(document.querySelector('#Title')).text(val);
-    if(count<=items[0].Url.length){
+    if(count<=10){
       id_arr.push(index);
-      if(count < items[0].Url.length){
+      var first = items[count-2].Index;
+      var second = items[count-1].Index;
+      if(count < 10){
         choise_arr.length = 0;
         choise_arr.push.apply(choise_arr, []);
-        for(i=0;i<2;i++){
-          choise_arr.push({"Url":items[i].Url[count] ,"Title":items[i].Title,"Id":items[i].Id});
+        for(i=count;i<(count+2);i++){
+          choise_arr.push({"ImageUrl":items[i].ImageUrl[0] ,"Name":items[i].Name,"Index":items[i].Index});
         }
+      index_arr.push(first ,second);
         $scope.choise = choise_arr;
       }
-      count++;
+      count = count + 2;
     }
-    if(count>items[0].Url.length){
+    if(count>10){
       $http({
         method: 'POST',
         url: '/api/',
-        data: id_arr,
+        data: index_arr,id_arr,
         headers: {"Content-Type":'application/x-www-form-urlencoded'}
       }).
       success(function(data, status, headers, config) {
         console.log("send to server");
       });
+	window.location.href = "/template/output.html";
     }
-    console.log("log:" + id_arr);
+    console.log("versus:" + index_arr);
+    console.log("choised:" + id_arr);
   }
 });
--- test.go
+++ test.go
@@ -1,10 +1,23 @@
 package hello
 
 import (
+	//	"appengine"
+	"encoding/json"
 	"fmt"
+	//"io"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"text/template"
 )
+
+type AllList struct {
+	Index            string
+	Name             string
+	ArticleDetailUrl string
+	ImageUrl         []string
+}
 
 func init() {
 	http.HandleFunc("/", handler)
@@ -12,13 +25,42 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is API endpoint root.")
+	//	c := appengine.NewContext(r)
+
+	var AllLists []AllList
+	var SelectLists []AllList
+
+	data, err := ioutil.ReadFile("./template/save_person_details.json")
+	data_err := json.Unmarshal(data, &AllLists)
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal(data_err)
+	}
+
+	/*	fmt.Fprintln(w, AllLists)
+		fmt.Fprintln(w, len(AllLists))
+		fmt.Fprintln(w, AllLists[331].Name)
+		fmt.Fprintln(w, AllLists[331].ImageUrl[0])
+	*/
+	for i := 0; i < 10; i++ {
+		SelectLists := AllLists[i]
+	}
+
+	values := url.Values{}
+	values.Add("0", "SelectLists")
+
+	resp, err := http.PostForm("http://localhost:8080/template/test.js/post", values)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
 
 func input(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("template/sample.html")
+	fmt.Fprintf(w, "this is API endpoint root.")
+	/*t, err := template.ParseFiles("template/sample.html")
 	if err == nil {
 		contents := make(map[string]string, 1)
 		t.Execute(w, contents)
-	}
+	}*/
 }
